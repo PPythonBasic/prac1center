1e785b640aec401429cc0c0ad617b7164b72c581
watchakorn-18k <74919942+watchakorn-18k@users.noreply.github.com>
2024-01-03 18:28:34 +0700

    feat: add solve

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # กำหนดให้ text = " Welcome " โดย print ออกมาให้อยู่กึ่งกลาง โดยมีความกว้าง 40 และมีตัวอักษรคือ # จากนั้นให้ผู้ใช้งานกรอกข้อมูลเข้ามาแล้วแสดงข้อมูลปกติ
-
+text = " Welcome "
 
 ## print
-
+print(text.center(40,"#"))
 
 ## รับข้อมูลจากผู้ใช้งาน
-
+user = input("Enter your name : ")
 
 ## print
+print("Hello",user)
